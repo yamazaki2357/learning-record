22451b1a27439981598a0185f87827733194826b
yamazaki2357 <yamazaki2357@gmail.com>
2023-01-05 21:44:05 +0900

    タスクの追加機能(ボタン)

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -13,8 +13,23 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button")
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  // button（削除）タグ生成
+  const deleteButton = document.createElement("button")
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // ulタグの子要素に各要素を設定
   ul.appendChild(li);
+  ul.appendChild(completeButton);
+  ul.appendChild(deleteButton);
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(ul);
